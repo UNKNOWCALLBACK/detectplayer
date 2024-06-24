1f0c2a91367feb06fbd03bc9b3ff174dcaf24f9a
UNKNOWCALLBACK_TEAM <aliencity9966@gmail.com>
2024-06-24 13:31:41 +0700

    first commit

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+server_srcipts {
+  'config/server/function/*.lua',
+    'config/server/*.lua',
+    'core/server/*.lua'
+
+}
+
+author 'UNKNOWCALLBACK'
+
+version '1.0'
